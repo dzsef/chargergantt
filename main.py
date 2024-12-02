2a61222db8f6be6477ae8af70dd59782c07d0756
dzsef <105501847+dzsef@users.noreply.github.com>
2024-12-02 18:43:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ html_file_path = "gantt_chart.html"
 with open(html_file_path, 'r', encoding='utf-8') as f:
     html_content = f.read()
 
-st.components.v1.html(html_content, height=600, scrolling=True)
+st.components.v1.html(html_content, width=1000, height= 800, scrolling=True)
