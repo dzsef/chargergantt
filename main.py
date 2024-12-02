97aef9696f264b14b397595dd4933aec177e51d0
dzsef <105501847+dzsef@users.noreply.github.com>
2024-12-02 18:29:05 +0100

    01- main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Vehicle Charging and Travel Schedule")
+st.write("Az alábbi Gantt diagram az elektromos járművek töltési és utazási ütemtervét mutatja be:")
+
+
+html_file_path = "gantt_chart.html"
+
+with open(html_file_path, 'r', encoding='utf-8') as f:
+    html_content = f.read()
+
+st.components.v1.html(html_content, height=600, scrolling=True)
